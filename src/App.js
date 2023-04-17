9d76a77652b17237a29f22191bea0e312a3263e9
Medrine-Jepkemoi <jepkemoimedrine@gmail.com>
2023-04-17 12:47:24 +0300

    image with description

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
-import logo from "./logo.svg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>First React App</h1>
+        <img
+          src={
+            "https://images.unsplash.com/photo-1681713670707-dab2de9965de?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+          }
+          className="App-logo"
+          alt="historical town"
+        />
+        <p>Historical old town with narrow alleys</p>
+        {/* <h1>First React App</h1> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
